dc4ffb47a478524c85ac31e783be1ec68ee8d864
Mangpha <8350130@gmail.com>
2022-07-18 17:56:09 +0900

    Apollo Codegen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
+import { LoginMutation, LoginMutationVariables } from "../__api__/LoginMutation";
 
 const LOGIN_MUTATION = gql`
     mutation LoginMutation($email: String!, $password: String!) {
@@ -25,7 +26,9 @@ export const Login = () => {
         formState: { errors },
         handleSubmit,
     } = useForm<ILoginForm>();
-    const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTATION);
+    const [loginMutation, { data }] = useMutation<LoginMutation, LoginMutationVariables>(
+        LOGIN_MUTATION,
+    );
     const onSubmit = () => {
         const { email, password } = getValues();
         loginMutation({
