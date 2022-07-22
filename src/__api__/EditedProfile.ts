5c350e87b09b6bb1cc2040e3547014d0da0d09bb
Mangpha <8350130@gmail.com>
2022-07-22 10:53:49 +0900

    Restaurants Query

---
+++ src/__api__/EditedProfile.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: EditedProfile
+// ====================================================
+
+export interface EditedProfile {
+  __typename: "User";
+  verified: boolean;
+  email: string;
+}
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,7 +1,54 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { CustomHelmet } from "../../components/helmet";
+import {
+    RestaurantsPageQuery,
+    RestaurantsPageQueryVariables,
+} from "../../__api__/RestaurantsPageQuery";
+
+const RESTAURANTS_QUERY = gql`
+    query RestaurantsPageQuery($input: RestaurantsInput!) {
+        allCategories {
+            categories {
+                id
+                name
+                coverImg
+                slug
+                restaurantCount
+            }
+        }
+        restaurants(input: $input) {
+            ok
+            error
+            totalPages
+            totalResults
+            results {
+                id
+                name
+                coverImg
+                address
+                isPromoted
+                category {
+                    name
+                }
+            }
+        }
+    }
+`;
 
 export const Restaurants = () => {
+    const { data, loading } = useQuery<RestaurantsPageQuery, RestaurantsPageQueryVariables>(
+        RESTAURANTS_QUERY,
+        {
+            variables: {
+                input: {
+                    page: 1,
+                },
+            },
+        },
+    );
+    console.log(data);
+
     return (
         <div>
             <CustomHelmet content="Restaurants" />
