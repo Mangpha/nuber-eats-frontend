9b68014c838af733ea461776cedad5a58341f232
Mangpha <8350130@gmail.com>
2022-07-27 11:50:20 +0900

    Button Tests

---
+++ src/components/__tests__/button.spec.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Button } from "../button";
+
+describe("<Button />", () => {
+    it("should render OK with props", () => {
+        render(<Button canClick={true} loading={false} actionText={"test"} />);
+        screen.getByText("test");
+    });
+
+    it("should display loading", () => {
+        const { debug, container } = render(
+            <Button canClick={false} loading={true} actionText={"test"} />,
+        );
+        screen.getByText("Loading...");
+        expect(container.firstChild).toHaveClass("pointer-events-none");
+    });
+});
