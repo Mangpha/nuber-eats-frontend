b2b76edf298c235fd59905133e8d18d42d92c3d7
Mangpha <8350130@gmail.com>
2022-07-27 11:58:24 +0900

    Form Error Tests

--- src/components/__tests__/form-error.spec.tsx
+++ src/components/__tests__/form-error.spec.tsx
@@ -4,7 +4,7 @@ import { FormError } from "../form-error";
 
 describe("<FormError />", () => {
     it("renders OK with props", () => {
-        const { debug } = render(<FormError errorMessage="test" />);
-        debug();
+        const { getByText } = render(<FormError errorMessage="test" />);
+        getByText("test");
     });
 });
