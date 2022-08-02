6227d49ed48e2da3ce5133cbb6a503b9942c1548
Mangpha <8350130@gmail.com>
2022-08-02 16:51:26 +0900

    Dish Options

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,5 +1,5 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "../../components/button";
@@ -20,6 +20,7 @@ interface IFormProps {
     name: string;
     price: string;
     description: string;
+    [key: string]: string;
 }
 
 export const AddDish = () => {
@@ -40,11 +41,15 @@ export const AddDish = () => {
             },
         ],
     });
-    const { register, handleSubmit, getValues, formState } = useForm<IFormProps>({
+    const { register, handleSubmit, getValues, formState, setValue } = useForm<IFormProps>({
         mode: "onChange",
     });
     const onSubmit = () => {
-        const { name, price, description } = getValues();
+        const { name, price, description, ...rest } = getValues();
+        const optionObjects = optionsLength.map((optionId) => ({
+            name: rest[`${optionId}-optionName`],
+            extra: +rest[`${optionId}-optionExtra`] || 0,
+        }));
         createDishMutation({
             variables: {
                 input: {
@@ -52,10 +57,20 @@ export const AddDish = () => {
                     price: +price,
                     description,
                     restaurantId: +(restaurantId + ""),
+                    options: optionObjects,
                 },
             },
         });
         navigate(-1);
+    };
+    const [optionsLength, setOptionsLength] = useState<number[]>([]);
+    const onAddOptionClick = () => {
+        setOptionsLength((cur) => [Date.now(), ...cur]);
+    };
+    const onDeleteOptionClick = (deleteId: number) => {
+        setOptionsLength((cur) => cur.filter((id) => id !== deleteId));
+        setValue(`${deleteId}-optionName`, "");
+        setValue(`${deleteId}-optionExtra`, "");
     };
 
     return (
@@ -88,6 +103,41 @@ export const AddDish = () => {
                     name="description"
                     placeholder="Description"
                 />
+                <div className="mt-5 mb-10 flex flex-col items-center">
+                    <h4 className="font-medium mb-1 text-lg">Dish Options</h4>
+                    <span
+                        onClick={onAddOptionClick}
+                        className="cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5"
+                    >
+                        Add Dish Option
+                    </span>
+                    {optionsLength.length !== 0 &&
+                        optionsLength.map((id) => (
+                            <div key={id} className="mt-5">
+                                <input
+                                    {...register(`${id}-optionName`)}
+                                    className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2 mr-3"
+                                    type="text"
+                                    name={`${id}-optionName`}
+                                    placeholder="Option Name"
+                                />
+                                <input
+                                    {...register(`${id}-optionExtra`)}
+                                    className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2 mr-3"
+                                    type="number"
+                                    name={`${id}-optionExtra`}
+                                    min={0}
+                                    placeholder="Option Extra"
+                                />
+                                <span
+                                    className="cursor-pointer text-white bg-red-500 ml-3 py-3 px-4 mt-5"
+                                    onClick={() => onDeleteOptionClick(id)}
+                                >
+                                    X
+                                </span>
+                            </div>
+                        ))}
+                </div>
                 <Button canClick={formState.isValid} loading={loading} actionText="Create Dish" />
             </form>
         </div>
