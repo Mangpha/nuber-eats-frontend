fabbc6e9f48ef8b8da6b22d898d6f738d41aa267
Mangpha <8350130@gmail.com>
2022-08-11 16:20:46 +0900

    React google map Setup

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
 
 export const Dashboard = () => {
     return (
         <div>
-            <h1>Dash Board</h1>
+            <div className="overflow-hidden" style={{ width: window.innerWidth, height: "95vh" }}>
+                <GoogleMapReact
+                    defaultZoom={20}
+                    defaultCenter={{
+                        lat: 10.99835602,
+                        lng: 77.01502627,
+                    }}
+                    bootstrapURLKeys={{ key: "AIzaSyBubq_cxGalUXvsrHIADJ02hakJnm2ywN4" }}
+                >
+                    <h1>Hello</h1>
+                </GoogleMapReact>
+            </div>
         </div>
     );
 };
