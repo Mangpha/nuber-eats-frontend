9bd6bec2ecea4accb506eb3cd22a51f779d66761
Mangpha <8350130@gmail.com>
2022-08-11 17:50:26 +0900

    Driver Dashboard

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 
+interface ICoords {
+    latitude: number;
+    longitude: number;
+}
+
 export const Dashboard = () => {
+    const [driverCoords, setDriverCoords] = useState<ICoords>({ latitude: 0, longitude: 0 });
+    const onSuccess = ({ coords: { latitude, longitude } }: GeolocationPosition) => {
+        setDriverCoords({ latitude, longitude });
+    };
+    const onError = (error: GeolocationPositionError) => {
+        console.log(error);
+    };
+    useEffect(() => {
+        navigator.geolocation.watchPosition(onSuccess, onError, { enableHighAccuracy: true });
+    }, []);
+
+    const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+        setTimeout(() => {
+            console.log(driverCoords);
+            map.panTo(new maps.LatLng(driverCoords.latitude, driverCoords.longitude));
+        }, 2000);
+    };
+
     return (
         <div>
             <div className="overflow-hidden" style={{ width: window.innerWidth, height: "95vh" }}>
                 <GoogleMapReact
-                    defaultZoom={20}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={onApiLoaded}
+                    defaultZoom={15}
                     defaultCenter={{
-                        lat: 10.99835602,
-                        lng: 77.01502627,
+                        lat: 35.8433904,
+                        lng: 128.530017,
                     }}
                     bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_MAP_KEY}` }}
-                >
-                    <h1>Hello</h1>
-                </GoogleMapReact>
+                ></GoogleMapReact>
             </div>
         </div>
     );
